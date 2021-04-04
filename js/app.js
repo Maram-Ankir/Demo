1cc774cd601b1b5657f521c7987e5f782a8eb594
Maram-Ankir <maramankir5@gmail.com>
2021-04-04 17:02:07 +0300

    add demo

---
+++ js/app.js
@@ -0,0 +1,23 @@
+
+ let userName = prompt('what\'s your name?');
+ console.log( userName);
+
+ let userAge = prompt("what's your age?");
+ console.log(userAge);
+
+ let favChoco = prompt("whats your favourite chocolate /black or white ?");
+ console.log(favChoco);
+
+ let userWeigh = prompt('how much you weigh?');
+ console.log(userWeigh );
+
+
+ alert('Your name is ' + userName + ' and your age is ' + userAge + ' your favourite chocolate is ' + favChoco + ' your weigh is ' + userWeigh);
+
+ console.log(typeof userName);
+ console.log(typeof userAge);
+ console.log(typeof favChoco);
+ console.log(typeof favChoco);
+
+
+
